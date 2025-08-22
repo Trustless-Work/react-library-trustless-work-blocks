4384d1bebcbae64bc521735b45a1895fcb07bf8c
Joel Vargas <vermudo.com@gmail.com>
2025-08-22 17:29:20 -0600

    feat: add DisputeEscrowButton component for initiating disputes in single-release escrow, including loading state and error handling

---
+++ templates/escrows/single-release/dispute-escrow/button/DisputeEscrow.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { Button } from "__UI_BASE__/button";
+import { useEscrowsMutations } from "@/components/tw-blocks/tanstak/useEscrowsMutations";
+import { useWalletContext } from "@/components/tw-blocks/wallet-kit/WalletProvider";
+import { SingleReleaseStartDisputePayload } from "@trustless-work/escrow/types";
+import { toast } from "sonner";
+import {
+  ErrorResponse,
+  handleError,
+} from "@/components/tw-blocks/handle-errors/handle";
+import { useEscrowContext } from "../../../escrow-context/EscrowProvider";
+import { Loader2 } from "lucide-react";
+
+export default function DisputeEscrowButton() {
+  const { startDispute } = useEscrowsMutations();
+  const { escrow } = useEscrowContext();
+  const { walletAddress } = useWalletContext();
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+  async function handleClick() {
+    try {
+      setIsSubmitting(true);
+
+      const payload: SingleReleaseStartDisputePayload = {
+        contractId: escrow?.contractId || "",
+        signer: walletAddress || "",
+      };
+
+      await startDispute.mutateAsync({
+        payload,
+        type: "single-release",
+        address: walletAddress || "",
+      });
+
+      toast.success("Escrow disputed successfully");
+    } catch (error) {
+      toast.error(handleError(error as ErrorResponse).message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <Button
+      type="button"
+      disabled={isSubmitting}
+      onClick={handleClick}
+      className="cursor-pointer"
+    >
+      {isSubmitting ? (
+        <div className="flex items-center">
+          <Loader2 className="h-5 w-5 animate-spin" />
+          <span className="ml-2">Disputing...</span>
+        </div>
+      ) : (
+        "Dispute Escrow"
+      )}
+    </Button>
+  );
+}
